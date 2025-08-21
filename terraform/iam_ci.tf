6119267342e75b43eed05fe41378dbc5737b0773
Oluwaseyi <abiolaseyip@gmail.com>
2025-08-21 18:43:25 +0100

    ci: kick run

--- terraform/iam_ci.tf
+++ terraform/iam_ci.tf
@@ -1,5 +1,4 @@
 # Trust policy for CI plan role (read-only)
-
 data "aws_iam_policy_document" "gha_oidc_trust" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -16,75 +15,85 @@ data "aws_iam_policy_document" "gha_oidc_trust" {
       values   = ["sts.amazonaws.com"]
     }
 
-    # Only this repository
+    # Only this repository (permit both casings)
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:repository"
-      values   = ["larriephill/url-shortener"]
+      values = [
+        "larriephill/url-shortener",
+        "Larriephill/url-shortener",
+      ]
     }
 
-    # Only this workflow name (matches 'name:' at top of ci.yml)
+    # Only this workflow name (matches `name:` in ci.yml)
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:workflow"
       values   = ["ci-dev"]
     }
 
-    # Allow branch runs on dev and pull_request runs
+    # Allow branch runs on dev and pull_request runs (both casings)
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
         "repo:larriephill/url-shortener:ref:refs/heads/dev",
-        "repo:larriephill/url-shortener:pull_request"
+        "repo:Larriephill/url-shortener:ref:refs/heads/dev",
+        "repo:larriephill/url-shortener:pull_request",
+        "repo:Larriephill/url-shortener:pull_request",
       ]
     }
   }
 }
 
-
-
 resource "aws_iam_role" "gha_plan" {
   name               = "url-dev-gha-plan"
   assume_role_policy = data.aws_iam_policy_document.gha_oidc_trust.json
-  tags = {
-    Project = "url-shortener",
-    Stage   = var.stage
-  }
 }
 
-# Permissions:
-# 1) ReadOnlyAccess so 'terraform plan' can read AWS resources
-# 2) Backend state access (S3 + DynamoDB lock)
-resource "aws_iam_role_policy_attachment" "read_only" {
-  role       = aws_iam_role.gha_plan.name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
-
-data "aws_iam_policy_document" "tf_backend" {
+# Minimal permissions for Terraform remote backend (S3+DDB lock)
+data "aws_iam_policy_document" "gha_plan_backend" {
   statement {
-    sid     = "StateBucketRW"
-    actions = ["s3:GetObject", "s3:PutObject", "s3:ListBucket", "s3:GetBucketLocation"]
+    sid     = "S3ListBucket"
+    effect  = "Allow"
+    actions = ["s3:ListBucket"]
     resources = [
-      "arn:aws:s3:::url-shortener-tfstate-larriephill-dev",  # bucket
-      "arn:aws:s3:::url-shortener-tfstate-larriephill-dev/*" # objects
+      "arn:aws:s3:::urlshortenerlarriephill"
     ]
   }
+
   statement {
-    sid       = "DynamoLockRW"
-    actions   = ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem", "dynamodb:DescribeTable"]
-    resources = [aws_dynamodb_table.tf_lock.arn]
+    sid     = "S3GetState"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    resources = [
+      "arn:aws:s3:::urlshortenerlarriephill/*"
+    ]
+  }
+
+  # DDB table used for state locking during plan
+  statement {
+    sid    = "DDBStateLock"
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [
+      "arn:aws:dynamodb:eu-west-2:416162027738:table/tf-lock-dev"
+    ]
   }
 }
 
-resource "aws_iam_policy" "tf_backend" {
-  name   = "url-dev-tf-backend-access"
-  policy = data.aws_iam_policy_document.tf_backend.json
+resource "aws_iam_policy" "gha_plan_backend" {
+  name        = "url-dev-gha-plan-backend"
+  description = "Read S3 backend + DDB lock permissions for plan"
+  policy      = data.aws_iam_policy_document.gha_plan_backend.json
 }
 
-resource "aws_iam_role_policy_attachment" "attach_backend" {
+resource "aws_iam_role_policy_attachment" "gha_plan_attach" {
   role       = aws_iam_role.gha_plan.name
-  policy_arn = aws_iam_policy.tf_backend.arn
+  policy_arn = aws_iam_policy.gha_plan_backend.arn
 }
-
-output "gha_plan_role_arn" { value = aws_iam_role.gha_plan.arn }
