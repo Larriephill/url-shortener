dae121160afabd8aa747978d9e9bc0bb5c44ea5d
Oluwaseyi <abiolaseyip@gmail.com>
2025-09-30 13:23:45 +0100

    updated IAM permissons to enable prod workflow run

--- terraform/iam_ci_apply_prod.tf
+++ terraform/iam_ci_apply_prod.tf
@@ -112,11 +112,16 @@ data "aws_iam_policy_document" "gha_apply_app_prod" {
     sid    = "LambdaManage"
     effect = "Allow"
     actions = [
-      "lambda:CreateFunction", "lambda:UpdateFunctionCode", "lambda:UpdateFunctionConfiguration",
-      "lambda:DeleteFunction", "lambda:Get*", "lambda:List*",
-      "lambda:PublishVersion", "lambda:CreateAlias", "lambda:UpdateAlias", "lambda:DeleteAlias",
-      "lambda:AddPermission", "lambda:RemovePermission",
-      "lambda:TagResource", "lambda:UntagResource"
+      "logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutRetentionPolicy",
+      "logs:DeleteLogGroup", "logs:DescribeLogGroups", "logs:DescribeLogStreams",
+      "logs:ListTagsForResource",
+      "cloudwatch:PutMetricAlarm", "cloudwatch:DeleteAlarms", "cloudwatch:DescribeAlarms",
+      "cloudwatch:TagResource", "cloudwatch:UntagResource",
+      "cloudwatch:ListTagsForResource",
+      # dashboard reads/writes
+      "cloudwatch:GetDashboard",
+      "cloudwatch:PutDashboard",
+      "cloudwatch:DeleteDashboards"
     ]
     resources = ["*"]
     condition {
@@ -162,7 +167,10 @@ data "aws_iam_policy_document" "gha_apply_app_prod" {
       "logs:ListTagsForResource",
       "cloudwatch:PutMetricAlarm", "cloudwatch:DeleteAlarms", "cloudwatch:DescribeAlarms",
       "cloudwatch:TagResource", "cloudwatch:UntagResource",
-      "cloudwatch:ListTagsForResource"
+      "cloudwatch:ListTagsForResource",
+      "cloudwatch:GetDashboard",
+      "cloudwatch:PutDashboard",
+      "cloudwatch:DeleteDashboards"
     ]
     resources = ["*"]
     condition {
@@ -261,4 +269,59 @@ resource "aws_iam_role_policy_attachment" "gha_apply_attach_self_desc_prod" {
   count      = local.is_prod ? 1 : 0
   role       = aws_iam_role.gha_apply_prod[0].name
   policy_arn = aws_iam_policy.gha_apply_self_desc_prod[0].arn
+}
+
+# ---------- DNS (Route53) + ACM for custom domain (prod) ----------
+# Route53: list/find zone and change records
+# ACM: request + describe certificate in eu-west-2
+
+data "aws_iam_policy_document" "gha_apply_dns_acm_prod" {
+  statement {
+    sid    = "Route53ManageApiRecords"
+    effect = "Allow"
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListHostedZonesByName",
+      "route53:GetHostedZone",
+      "route53:ListResourceRecordSets",
+      "route53:ChangeResourceRecordSets",
+      "route53:ListTagsForResource",
+    ]
+    resources = ["*"] # Route53 APIs are mostly not resource-scoped
+  }
+
+  statement {
+    sid    = "AcmRequestAndDescribe"
+    effect = "Allow"
+    actions = [
+      "acm:RequestCertificate",
+      "acm:DescribeCertificate",
+      "acm:ListCertificates",
+      "acm:AddTagsToCertificate",
+      "acm:ListTagsForCertificate",
+      # TF to delete unused certs
+      "acm:DeleteCertificate",
+      "acm:RemoveTagsFromCertificate"
+    ]
+    resources = ["*"]
+    condition {
+      # ACM is regional; keep it constrained
+      test     = "StringEquals"
+      variable = "aws:RequestedRegion"
+      values   = [data.aws_region.current.name]
+    }
+  }
+}
+
+resource "aws_iam_policy" "gha_apply_dns_acm_prod" {
+  count       = local.is_prod ? 1 : 0
+  name        = "url-prod-gha-apply-dns-acm"
+  description = "Manage Route53 records and ACM certificates for custom API domain (prod)"
+  policy      = data.aws_iam_policy_document.gha_apply_dns_acm_prod.json
+}
+
+resource "aws_iam_role_policy_attachment" "gha_apply_attach_dns_acm_prod" {
+  count      = local.is_prod ? 1 : 0
+  role       = aws_iam_role.gha_apply_prod[0].name
+  policy_arn = aws_iam_policy.gha_apply_dns_acm_prod[0].arn
 }
